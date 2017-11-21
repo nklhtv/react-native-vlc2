b6410679ab3fbd672b76ed8d320b2a6a9b78e70b
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2017-11-21 15:08:02 +0200

    VLCVideoEventEmitter access modifiers changed

--- android/src/main/java/com/stellarscript/vlcvideo/VLCVideoEventEmitter.java
+++ android/src/main/java/com/stellarscript/vlcvideo/VLCVideoEventEmitter.java
@@ -10,51 +10,51 @@ final class VLCVideoEventEmitter {
     private final VLCVideoView mVideoView;
     private final RCTEventEmitter mEventEmitter;
 
-    public VLCVideoEventEmitter(final VLCVideoView videoView, final ThemedReactContext themedReactContext) {
+    VLCVideoEventEmitter(final VLCVideoView videoView, final ThemedReactContext themedReactContext) {
         mVideoView = videoView;
         mEventEmitter = themedReactContext.getJSModule(RCTEventEmitter.class);
     }
 
-    public void emitOnBuffering(final double buffering) {
+    void emitOnBuffering(final double buffering) {
         final WritableMap event = Arguments.createMap();
         event.putDouble(VLCVideoEvents.ON_BUFFERING_BUFFERING_PROP, buffering);
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_BUFFERING_EVENT, event);
     }
 
-    public void emitOnPlaying(final double duration) {
+    void emitOnPlaying(final double duration) {
         final WritableMap event = Arguments.createMap();
         event.putDouble(VLCVideoEvents.ON_PLAYING_DURATION_PROP, duration);
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_PLAYING_EVENT, event);
     }
 
-    public void emitOnPaused() {
+    void emitOnPaused() {
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_PAUSED_EVENT, null);
     }
 
-    public void emitOnEndReached() {
+    void emitOnEndReached() {
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_END_REACHED_EVENT, null);
     }
 
-    public void emitOnError(final String message, final boolean isCritical) {
+    void emitOnError(final String message, final boolean isCritical) {
         final WritableMap event = Arguments.createMap();
         event.putString(VLCVideoEvents.ON_ERROR_MESSAGE_PROP, message);
         event.putBoolean(VLCVideoEvents.ON_ERROR_IS_CRITICAL_PROP, isCritical);
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_ERROR_EVENT, event);
     }
 
-    public void emitOnTimeChanged(final double currentTime) {
+    void emitOnTimeChanged(final double currentTime) {
         final WritableMap event = Arguments.createMap();
         event.putDouble(VLCVideoEvents.ON_TIME_CHANGED_TIME_PROP, currentTime);
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_TIME_CHANGED_EVENT, event);
     }
 
-    public void emitOnSeekRequested(final double time) {
+    void emitOnSeekRequested(final double time) {
         final WritableMap event = Arguments.createMap();
         event.putDouble(VLCVideoEvents.ON_SEEK_REQUESTED_TIME_PROP, time);
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_SEEK_REQUESTED_EVENT, event);
     }
 
-    public void emitOnSeekPerformed() {
+    void emitOnSeekPerformed() {
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_SEEK_PERFORMED_EVENT, null);
     }
 
