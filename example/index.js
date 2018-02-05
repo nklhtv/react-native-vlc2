fc02d4c434e8150632898fdaeb5cdb1cd0a25f85
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-02-05 13:39:31 +0200

    render two videos in example

--- example/index.js
+++ example/index.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { View, AppRegistry } from 'react-native';
 import VLCVideo from 'react-native-vlc2';
 
 class App extends Component {
     render() {
         return (
-            <VLCVideo
-                ref={'video'}
-                style={{flex: 1, backgroundColor: 'green'}}
-                sourceUrl={'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'}
-                autoplay={true}
-                startTime={0}
-            />
+            <View style={{flex: 1, backgroundColor: 'green'}}>
+                <VLCVideo
+                    style={{flex: 1}}
+                    sourceUrl={'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'}
+                    autoplay={true}
+                    startTime={0}
+                />
+                <VLCVideo
+                    style={{flex: 1}}
+                    sourceUrl={'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'}
+                    autoplay={true}
+                    startTime={0}
+                />
+            </View>
         );
     }
 }
