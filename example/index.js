975cb90064a317d8e9fcb66e8e3bc0607b84a09c
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-02-05 12:31:40 +0200

    example renders fullscreen video

--- example/index.js
+++ example/index.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import { AppRegistry, View } from 'react-native';
+import { AppRegistry } from 'react-native';
 import VLCVideo from 'react-native-vlc2';
 
 class App extends Component {
     render() {
         return (
-            <View style={{flex: 1, backgroundColor: 'green'}}>
-                <VLCVideo
-                    ref={'video'}
-                    style={{ height: 100, width: '100%' }}
-                    sourceUrl={'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'}
-                    autoplay={true}
-                    startTime={0}
-                />
-            </View>
+            <VLCVideo
+                ref={'video'}
+                style={{flex: 1, backgroundColor: 'green'}}
+                sourceUrl={'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'}
+                autoplay={true}
+                startTime={0}
+            />
         );
     }
 }
