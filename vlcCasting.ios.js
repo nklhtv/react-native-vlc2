ea3083b215e4c3e92118f533aeb4afde85e97129
nklhrstv <me@nikolahristov.com>
2021-05-21 09:10:03 -0400

    match android api in ios code split

--- vlcCasting.ios.js
+++ vlcCasting.ios.js
@@ -6,12 +6,32 @@ const removeOnRenderersChangeListener = (cb) => {
     throw new Error('Not implemented!');
 };
 
+const addOnSelectedRendererChangeListener = (cb) => {
+    throw new Error('Not implemented!');
+};
+
+const removeOnSelectedRendererChangeListener = (cb) => {
+    throw new Error('Not implemented!');
+};
+
 const getRenderers = (cb) => {
+    throw new Error('Not implemented!');
+};
+
+const getSelectedRenderer = (cb) => {
+    throw new Error('Not implemented!');
+};
+
+const setSelectedRenderer = (renderer) => {
     throw new Error('Not implemented!');
 };
 
 export default {
     getRenderers,
+    getSelectedRenderer,
+    setSelectedRenderer,
     addOnRenderersChangeListener,
-    removeOnRenderersChangeListener
+    removeOnRenderersChangeListener,
+    addOnSelectedRendererChangeListener,
+    removeOnSelectedRendererChangeListener
 };
