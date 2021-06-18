7ba0f82d1976d69d18cff2154750539734de3896
nklhrstv <me@nikolahristov.com>
2021-06-18 14:28:59 +0300

    revert making vlc video event emitter public

--- android/src/main/java/com/stremio/vlc/video/VLCVideoEventEmitter.java
+++ android/src/main/java/com/stremio/vlc/video/VLCVideoEventEmitter.java
@@ -1,7 +1,5 @@
 package com.stremio.vlc.video;
 
-import android.view.View;
-
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
@@ -10,60 +8,60 @@ import com.facebook.react.uimanager.events.RCTEventEmitter;
 
 import org.videolan.libvlc.MediaPlayer.TrackDescription;
 
-public final class VLCVideoEventEmitter {
+final class VLCVideoEventEmitter {
 
-    private final View mVideoView;
+    private final VLCVideoView mVideoView;
     private final RCTEventEmitter mEventEmitter;
 
-    public VLCVideoEventEmitter(final View videoView, final ThemedReactContext themedReactContext) {
+    VLCVideoEventEmitter(final VLCVideoView videoView, final ThemedReactContext themedReactContext) {
         mVideoView = videoView;
         mEventEmitter = themedReactContext.getJSModule(RCTEventEmitter.class);
     }
 
-    public void emitOnBuffering(final long buffering) {
+    void emitOnBuffering(final long buffering) {
         final WritableMap event = Arguments.createMap();
         event.putDouble(VLCVideoEvents.BUFFERING_PROP, buffering);
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_BUFFERING_EVENT, event);
     }
 
-    public void emitOnPlaying(final long duration) {
+    void emitOnPlaying(final long duration) {
         final WritableMap event = Arguments.createMap();
         event.putDouble(VLCVideoEvents.DURATION_PROP, duration);
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_PLAYING_EVENT, event);
     }
 
-    public void emitOnPaused() {
+    void emitOnPaused() {
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_PAUSED_EVENT, null);
     }
 
-    public void emitOnEndReached() {
+    void emitOnEndReached() {
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_END_REACHED_EVENT, null);
     }
 
-    public void emitOnError(final String message, final boolean isCritical) {
+    void emitOnError(final String message, final boolean isCritical) {
         final WritableMap event = Arguments.createMap();
         event.putString(VLCVideoEvents.MESSAGE_PROP, message);
         event.putBoolean(VLCVideoEvents.IS_CRITICAL_PROP, isCritical);
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_ERROR_EVENT, event);
     }
 
-    public void emitOnTimeChanged(final long time) {
+    void emitOnTimeChanged(final long time) {
         final WritableMap event = Arguments.createMap();
         event.putDouble(VLCVideoEvents.TIME_PROP, time);
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_TIME_CHANGED_EVENT, event);
     }
 
-    public void emitOnSeekRequested(final long time) {
+    void emitOnSeekRequested(final long time) {
         final WritableMap event = Arguments.createMap();
         event.putDouble(VLCVideoEvents.TIME_PROP, time);
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_SEEK_REQUESTED_EVENT, event);
     }
 
-    public void emitOnSeekPerformed() {
+    void emitOnSeekPerformed() {
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_SEEK_PERFORMED_EVENT, null);
     }
 
-    public void emitOnSubtitleTracksChanged(final TrackDescription[] tracks) {
+    void emitOnSubtitleTracksChanged(final TrackDescription[] tracks) {
         final WritableArray eventTracks = Arguments.createArray();
         if (tracks != null) {
             for (final TrackDescription track : tracks) {
@@ -83,7 +81,7 @@ public final class VLCVideoEventEmitter {
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_SUBTITLE_TRACKS_CHANGED_EVENT, event);
     }
 
-    public void emitOnAudioTracksChanged(final TrackDescription[] tracks) {
+    void emitOnAudioTracksChanged(final TrackDescription[] tracks) {
         final WritableArray eventTracks = Arguments.createArray();
         if (tracks != null) {
             for (final TrackDescription track : tracks) {
@@ -103,13 +101,13 @@ public final class VLCVideoEventEmitter {
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_AUDIO_TRACKS_CHANGED_EVENT, event);
     }
 
-    public void emitOnSelectedSubtitleTrackIdChanged(final int id) {
+    void emitOnSelectedSubtitleTrackIdChanged(final int id) {
         final WritableMap event = Arguments.createMap();
         event.putInt(VLCVideoEvents.TRACK_ID_PROP, id);
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_SELECTED_SUBTITLE_TRACK_ID_CHANGED_EVENT, event);
     }
 
-    public void emitOnSelectedAudioTrackIdChanged(final int id) {
+    void emitOnSelectedAudioTrackIdChanged(final int id) {
         final WritableMap event = Arguments.createMap();
         event.putInt(VLCVideoEvents.TRACK_ID_PROP, id);
         mEventEmitter.receiveEvent(mVideoView.getId(), VLCVideoEvents.ON_SELECTED_AUDIO_TRACK_ID_CHANGED_EVENT, event);
