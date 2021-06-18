4e56477eb7aeec80b5e48de72a16e8527e017010
nklhrstv <me@nikolahristov.com>
2021-06-18 14:32:57 +0300

    revert VLCVideoProps declared public

--- android/src/main/java/com/stremio/vlc/video/VLCVideoProps.java
+++ android/src/main/java/com/stremio/vlc/video/VLCVideoProps.java
@@ -1,32 +1,32 @@
 package com.stremio.vlc.video;
 
-public final class VLCVideoProps {
+final class VLCVideoProps {
 
-    public static final String MEDIA_PROP = "media";
-    public static final String MEDIA_SOURCE_URL_PROP = "sourceUrl";
-    public static final String MEDIA_START_TIME_PROP = "startTime";
-    public static final int MEDIA_START_TIME_DEFAULT_VALUE = 0;
-    public static final String MEDIA_AUTOPLAY_PROP = "autoplay";
-    public static final boolean MEDIA_AUTOPLAY_DEFAULT_VALUE = true;
-    public static final String MEDIA_HW_DECODER_ENABLED_PROP = "hwDecoderEnabled";
-    public static final boolean MEDIA_HW_DECODER_ENABLED_DEFAULT_VALUE = true;
-    public static final String MEDIA_TITLE_PROP = "title";
-    public static final String MEDIA_TITLE_DEFAULT_VALUE = "";
-    public static final String KEY_CONTROL_ENABLED_PROP = "keyControlEnabled";
-    public static final boolean KEY_CONTROL_ENABLED_DEFAULT_VALUE = false;
-    public static final String PLAY_IN_BACKGROUND_PROP = "playInBackground";
-    public static final boolean PLAY_IN_BACKGROUND_DEFAULT_VALUE = false;
+    static final String MEDIA_PROP = "media";
+    static final String MEDIA_SOURCE_URL_PROP = "sourceUrl";
+    static final String MEDIA_START_TIME_PROP = "startTime";
+    static final int MEDIA_START_TIME_DEFAULT_VALUE = 0;
+    static final String MEDIA_AUTOPLAY_PROP = "autoplay";
+    static final boolean MEDIA_AUTOPLAY_DEFAULT_VALUE = true;
+    static final String MEDIA_HW_DECODER_ENABLED_PROP = "hwDecoderEnabled";
+    static final boolean MEDIA_HW_DECODER_ENABLED_DEFAULT_VALUE = true;
+    static final String MEDIA_TITLE_PROP = "title";
+    static final String MEDIA_TITLE_DEFAULT_VALUE = "";
+    static final String KEY_CONTROL_ENABLED_PROP = "keyControlEnabled";
+    static final boolean KEY_CONTROL_ENABLED_DEFAULT_VALUE = false;
+    static final String PLAY_IN_BACKGROUND_PROP = "playInBackground";
+    static final boolean PLAY_IN_BACKGROUND_DEFAULT_VALUE = false;
 
-    public static final String PLAY_COMMAND_NAME = "play";
-    public static final int PLAY_COMMAND_ID = 1;
-    public static final String PAUSE_COMMAND_NAME = "pause";
-    public static final int PAUSE_COMMAND_ID = 2;
-    public static final String SEEK_COMMAND_NAME = "seek";
-    public static final int SEEK_COMMAND_ID = 3;
-    public static final String SET_SUBTITLE_TRACK_COMMAND_NAME = "setSubtitleTrack";
-    public static final int SET_SUBTITLE_TRACK_COMMAND_ID = 4;
-    public static final String SET_AUDIO_TRACK_COMMAND_NAME = "setAudioTrack";
-    public static final int SET_AUDIO_TRACK_COMMAND_ID = 5;
-    public static final int SEEK_COMMAND_TIME_ARGUMENT_INDEX = 0;
+    static final String PLAY_COMMAND_NAME = "play";
+    static final int PLAY_COMMAND_ID = 1;
+    static final String PAUSE_COMMAND_NAME = "pause";
+    static final int PAUSE_COMMAND_ID = 2;
+    static final String SEEK_COMMAND_NAME = "seek";
+    static final int SEEK_COMMAND_ID = 3;
+    static final String SET_SUBTITLE_TRACK_COMMAND_NAME = "setSubtitleTrack";
+    static final int SET_SUBTITLE_TRACK_COMMAND_ID = 4;
+    static final String SET_AUDIO_TRACK_COMMAND_NAME = "setAudioTrack";
+    static final int SET_AUDIO_TRACK_COMMAND_ID = 5;
+    static final int SEEK_COMMAND_TIME_ARGUMENT_INDEX = 0;
 
 }
