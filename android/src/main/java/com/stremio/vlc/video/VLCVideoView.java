a0908a1736e4b5938b861a8451d8c82ceb00a7dc
nklhrstv <me@nikolahristov.com>
2022-01-31 17:29:21 +0200

    use this keyword consistently

--- android/src/main/java/com/stremio/vlc/video/VLCVideoView.java
+++ android/src/main/java/com/stremio/vlc/video/VLCVideoView.java
@@ -420,7 +420,7 @@ public final class VLCVideoView extends VLCVideoLayout {
     private void attachVLCVoutViews() {
         final IVLCVout vout = mMediaPlayer.getVLCVout();
         if (!vout.areViewsAttached()) {
-            mMediaPlayer.attachViews(this, null, true, false);
+            mMediaPlayer.attachViews(VLCVideoView.this, null, true, false);
             mMediaPlayer.setVideoScale(MediaPlayer.ScaleType.SURFACE_BEST_FIT);
         }
     }
